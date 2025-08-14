29642f1271107ec23ebabfeb88dafdbfbff83fb4
Taariq Mullins <tjma2001@gmail.com>
2025-08-14 16:53:14 +0200

    refactor: updated cv

--- src/data/about.ts
+++ src/data/about.ts
@@ -1,59 +1,72 @@
 export const Qualifications = [
   {
-    institution: 'PhD',
-    date: 'July  2018 -',
-    description: 'Computer Science, UWC'
+    institution: "PhD",
+    date: "July  2018 -",
+    description: "Computer Science, UWC",
   },
   {
-    institution: 'MSc',
-    date: 'January  2013 - June 2014',
-    description: 'Computer Science, UCT'
+    institution: "MSc",
+    date: "January  2013 - June 2014",
+    description: "Computer Science, UCT",
   },
   {
-    institution: 'BSc Hons',
-    date: 'January  2012 - December 2012',
-    description: 'Computer Science, UCT'
+    institution: "BSc Hons",
+    date: "January  2012 - December 2012",
+    description: "Computer Science, UCT",
   },
   {
-    institution: 'BSc',
-    date: 'January 2004 -Dec 2007',
-    description: 'Computer Science, UCT'
-  }
+    institution: "BSc",
+    date: "January 2004 -Dec 2007",
+    description: "Computer Science, UCT",
+  },
 ]
 
 export const Experience = [
   {
-    dates: 'Septemper 2017 - Current',
-    title: 'Freelance',
-    titleDescription: 'Full Stack Software Engineer',
+    dates: "January 2024 - Present",
+    title: "CTO Orange",
+    titleDescription: "Chief Technology Officer ",
     description:
-      'Providing software services, kubernetes integrations and product development.'
+      "Leading the technology strategy and implementation for Orange Global Services.",
   },
   {
-    dates: 'July 2013 - August 2017',
-    title: 'WhereIsMyTransport, Cape Town',
-    titleDescription: 'Full Stack Software Engineer',
+    dates: "October 2021 - November 2023",
+    title: "Freelance",
+    titleDescription: "Full Stack Software Engineer",
+    description: "Created a browser based WebRTC game streaming platform",
+  },
+  {
+    dates: "September 2017 - November 2023",
+    title: "Freelance",
+    titleDescription: "Full Stack Software Engineer",
     description:
-      'Here I was responsible for many projects which I managed completely from the ground up. I had large roles to play in provisioning and management of critical infrastructure. Spinning up support web-services was another task I was often engaged in due to my speed and ease with which I handled these tasks.'
+      "Providing software services, kubernetes integrations and product development.",
   },
   {
-    dates: 'March 2010 - December 2011',
-    title: 'Bitfire, Cape Town',
-    titleDescription: 'Partner'
+    dates: "July 2013 - August 2017",
+    title: "WhereIsMyTransport, Cape Town",
+    titleDescription: "Full Stack Software Engineer",
+    description:
+      "Here I was responsible for many projects which I managed completely from the ground up. I had large roles to play in provisioning and management of critical infrastructure. Spinning up support web-services was another task I was often engaged in due to my speed and ease with which I handled these tasks.",
   },
   {
-    dates: 'Jan 2009 - March 2010',
-    title: 'Vodacom Gated Services, Cape Town',
-    titleDescription: 'Senior Network Engineer'
+    dates: "March 2010 - December 2011",
+    title: "Bitfire, Cape Town",
+    titleDescription: "Partner",
   },
   {
-    dates: 'June 2007 - January 2009',
-    title: 'SUGA Communication, Cape Town',
-    titleDescription: 'Partner'
-  }
+    dates: "Jan 2009 - March 2010",
+    title: "Vodacom Gated Services, Cape Town",
+    titleDescription: "Senior Network Engineer",
+  },
+  {
+    dates: "June 2007 - January 2009",
+    title: "SUGA Communication, Cape Town",
+    titleDescription: "Partner",
+  },
 ]
 
 export const ResearchPublications = [
-  'Service Provisioning for Lightweight Community Cloud Infrastructures, Conference: 2016 International Conference on Information Resources Management',
-  'Monitoring Community Clouds: The Lightweight Network Management Protocol, Conference: Proceedings of the 2013 IEEE 10th International Conference on Ubiquitous Intelligence & Computing and 2013 IEEE 10th International Conference on Autonomic & Trusted Computing'
+  "Service Provisioning for Lightweight Community Cloud Infrastructures, Conference: 2016 International Conference on Information Resources Management",
+  "Monitoring Community Clouds: The Lightweight Network Management Protocol, Conference: Proceedings of the 2013 IEEE 10th International Conference on Ubiquitous Intelligence & Computing and 2013 IEEE 10th International Conference on Autonomic & Trusted Computing",
 ]
